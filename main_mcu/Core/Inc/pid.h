2fd14474a4e4c33702ca5b233c4c72fa630ca5c2
BalasaiAnish <ponnaluribalasai@gmail.com>
2024-12-15 11:04:46 +0530

    added pid features

--- main_mcu/Core/Inc/pid.h
+++ main_mcu/Core/Inc/pid.h
@@ -8,6 +8,40 @@
 #ifndef INC_PID_H_
 #define INC_PID_H_
 
+#define IMU_CONTROL
 
+#include "stm32f4xx.h"
+
+typedef struct state_vars
+{
+    float x,y,z;
+}state_vars;
+
+/*
+typedef struct state_vars
+{
+    xyz linear, angular;
+}state_vars;
+*/
+
+typedef struct pid_consts
+{
+    float kp, kd, ki;
+}pid_consts;
+
+typedef struct pid_controller
+{
+    pid_consts consts;
+    state_vars prev_measurements, target;
+    float error_prev, error_sum;
+    int time_prev;
+
+}pid_controller;
+
+void pid_init(pid_controller *, pid_consts, state_vars);
+
+void pid_update(pid_controller *, state_vars);
+
+float pid_calculate(pid_controller *, float, float);
 
 #endif /* INC_PID_H_ */
--- main_mcu/Core/Src/pid.c
+++ main_mcu/Core/Src/pid.c
@@ -5,4 +5,30 @@
  *      Author: bala
  */
 
+#include "pid.h"
 
+void pid_init(pid_controller *pid_ctrl, pid_consts consts, state_vars initial_state)
+{
+    pid_ctrl->consts = consts;
+    pid_ctrl->prev_measurements = initial_state;
+}
+
+void pid_update(pid_controller *pid_ctrl, state_vars updated_target)
+{
+    pid_ctrl->target = updated_target;
+}
+
+float pid_calculate(pid_controller *pid_ctrl, float measurement, float target)
+{
+    float error = target - measurement;
+    
+    int dt = HAL_GetTick() - pid_ctrl->time_prev;
+
+    float error_diff = (error - pid_ctrl->error_prev)/dt;
+
+    pid_ctrl->error_sum += error;
+
+    float pid_res = pid_ctrl->consts.kp * error + pid_ctrl->consts.kd * error_diff + pid_ctrl->consts.ki * pid_ctrl->error_sum;
+
+    return pid_res;
+}
